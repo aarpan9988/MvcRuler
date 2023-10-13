ae2833f17a5d138f653682d382869a9f9bf8c97d
W0820839 <W0820839@ADL-A1201I-08>
2023-10-13 11:38:10 -0400

    i add validation in ruler model

--- Models/Ruler.cs
+++ Models/Ruler.cs
@@ -8,16 +8,30 @@ namespace MvcRuler.Models
     
         {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(4)]
+        [Required]
         public string Purpose { get; set; }
+
+        [StringLength(80, MinimumLength = 2)]
+        [Required]
         public string Material { get; set; }
 
         public decimal Length { get; set; }
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
 
 
